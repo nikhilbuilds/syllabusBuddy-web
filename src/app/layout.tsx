4d28a7eff31fbc239ba214cb1d032cbc1b0527cc
nikhilbuilds <devnikhil0306@gmail.com>
2025-06-08 09:15:27 +0530

    favicon updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import CssBaseline from "@mui/material/CssBaseline";
 import theme from "../theme";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,7 +19,6 @@ export default function RootLayout({
       <body className={inter.className}>
         <ThemeProvider theme={theme}>
           <CssBaseline />
-          <Navbar />
           {children}
           <Footer />
         </ThemeProvider>
